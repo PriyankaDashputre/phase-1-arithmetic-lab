5f841d3a0094639c8a4f207d612c791a659806cb
PriyankaDashputre <dashputre.priyanka@gmail.com>
2023-04-01 16:21:32 -0700

    Arithmatic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
+const random = Math.ceil(Math.random()* 10) + 1;
+
+const num3 = 8;
+const num4 = 2;
+const mod = 4
+num3 % num4 === 4;
+
+const max = Math.max(1, 4, 8, 12, 17, 19, 20);
 // Write your code here
